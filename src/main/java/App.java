d5c7276e25650e29eae30efab58fa9a6e572eaf2
Mikyas Tesfamichael <mickyastesfamichael@gmail.com>
2019-10-10 17:54:16 +0300

    deleted the getter for the DAO of member objects (direct static access used)
    this commit has nothing to do with the purpose of this branch (bear with me)

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,7 +3,6 @@ import view.GeneratorGUI;
 
 class ScheduleGenerator {
     public static void main(String[] args) {
-        SettingInitializer.initialize();
         System.setProperty("com.j256.ormlite.logger.level","INFO");
         new GeneratorGUI().setupAndDrawUI();
     }
